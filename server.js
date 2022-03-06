9dab524f25864017bf10786801d27ad593bcf047
patfern <patrick.j.fernbach@gmail.com>
2022-03-05 22:35:50 -0500

    updated port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { notes } = require('./db/db.json');
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -25,6 +26,6 @@ app.get('/notes', (req, res) => {
 
 
 
-app.listen(3001, ()=> {
+app.listen(PORT, ()=> {
     console.log(`API server now on port 3001!`);
 });
